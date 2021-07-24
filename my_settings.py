21491e41dfcddfd5a8629595809ead85f3907ff2
임종성 <qkqndudnxld@gmail.com>
2021-07-24 17:32:52 +0900

    Add: Setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'asdfasdf'
